b24d891927f4c5b48d9ed27be78c2c6528959776
Aleksandr A <aleksandra@MacBook-Air-Aleksandr.local>
2024-09-28 19:57:32 +0300

    ADD MAP

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -267,8 +267,13 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const arrEmpty = [];
+  arr.map((item, index) => {
+    arrEmpty.push(...Array(index + 1).fill(item));
+    return index;
+  });
+  return arrEmpty;
 }
 
 
@@ -472,8 +477,9 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const set = new Set(arr);
+  return Array.from(set);
 }
 
 /**
