a088466249202e90e7745931e01b44444fa6e0c5
Aleksandr A <aleksandra@MacBook-Air-Aleksandr.local>
2024-10-07 19:15:06 +0300

    getIntervalString

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -215,8 +215,12 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const sortNymb = [a, b].sort();
+  if (isStartIncluded && !isEndIncluded) return `[${sortNymb[0]}, ${sortNymb[1]})`;
+  if (!isStartIncluded && isEndIncluded) return `(${sortNymb[0]}, ${sortNymb[1]}]`;
+  if (!isStartIncluded && !isEndIncluded) return `(${sortNymb[0]}, ${sortNymb[1]})`;
+  return `[${sortNymb[0]}, ${sortNymb[1]}]`;
 }
 
 
